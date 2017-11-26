798715edfaa29ee59d8f1afde3e6a8f6d8dbc806
Ahmad Alstaty <ahmad.alstaty@gmail.com>
2017-11-26 10:41:02 +0200

    Auto-generated commit

--- styles.e04178593a61903399c7.bundle.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([2],{2:function(o,t,c){c("vjk9"),c("y3lt"),o.exports=c("sMuD")},sMuD:function(o,t,c){o.exports=c.p+"Gobold Extra1.38953a73440e17d9332f.otf"},vjk9:function(o,t){},y3lt:function(o,t,c){o.exports=c.p+"Gobold Bold.a213b3c5ccb42a31c042.otf"}},[2]);
